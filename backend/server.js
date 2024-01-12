d1f0a5d585dcce7d2f74813c19b44585d2aa5a58
Mesekir19 <mesekirsalgech@gmail.com>
2024-01-12 16:07:44 +0300

    server updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 const PORT = process.env.PORT || 5000;
 connectDb()
 
-app.use("/backend/employees", employeeRoute);
+app.use("/employees", employeeRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`.cyan);
